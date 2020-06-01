ea2642344d1c29c0ebd0001735527c6c4cc304ae
AnhTV <vanh.trinh.4869@gmail.com>
2020-06-01 21:21:21 +0700

    express is interesting

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var userRoute = require('./routes/user.route');
+
+var port = 9000;
+var app = express();
+
+app.set('view engine','pug');
+app.set('views','./views');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.use('/users',userRoute);
+
+app.listen(port, () => console.log(`Server running in port ${port}`));
+
+
+
+
+
+
+
