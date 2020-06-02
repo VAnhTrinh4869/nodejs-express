e96b06422901681103eacbb29d003be71dda765c
AnhTV <vanh.trinh.4869@gmail.com>
2020-06-02 22:19:20 +0700

    md5

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,5 @@
+var md5 = require('md5');
+
 var db = require('../db');
 
 module.exports.login = (req, res) => {
@@ -16,7 +18,7 @@ module.exports.postLogin = (req, res) => {
         });
         return;
     }
-    if(user.password !== password){
+    if(user.password !== md5(password)){
         res.render('auth/login',{
             errors: ['Wrong password.'],
             values: req.body
